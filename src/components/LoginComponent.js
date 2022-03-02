bbb261922bed3ad9635b34c19366cd87cc84fc37
NikitaPeshko <79310641+NikitaPeshko@users.noreply.github.com>
2022-03-02 10:40:53 +0300

    add validation to log In form

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -13,6 +13,7 @@ const LoginComponent=()=>{
     const [login, setLogin] = useState("");
     const [password, setPassword] = useState("");
     const [loader,setLoader]=useState(false);
+    const [loginOrPasswordIncorect,setLoginOrPasswordIncorect]=useState(true);
 
 
 
@@ -27,7 +28,7 @@ const LoginComponent=()=>{
         console.log(credentials);
         GiftServices.authantification(credentials).then(res=>{
             let token=res.data.token;
-            alert(token);
+            //alert(token);
             if(token!==undefined){
                 localStorage.setItem('userLogin',login)
                 localStorage.setItem('jwtToken', token)
@@ -38,14 +39,18 @@ const LoginComponent=()=>{
                 
 
                 
-                               
-
                 const history=createBrowserHistory();
                 history.push('/gifts');
                 history.go();
 
+            }else{
+                setLoginOrPasswordIncorect('');
+
             }
             
+            
+        }).catch(err=>{
+            setLoginOrPasswordIncorect('');
             
         });
        
@@ -75,17 +80,18 @@ const LoginComponent=()=>{
                     <div className = "row">
                         <div className = "card col-md-6 offset-md-3 offset-md-3">
                             <h3 className="text-center">Log In</h3>
+                            <h4 hidden={loginOrPasswordIncorect} style={{color:'red',textAlign:"center"}}>Login or password incorrect</h4>
                             <div className = "card-body">
                                 <form>
                                     <div className = "form-group">
                                         <label> Name: </label>
                                         <input placeholder="login" name="login" className="form-control" 
-                                            value={login} onChange={changeLoginHandler}/>
+                                            value={login} onChange={changeLoginHandler} required/>
                                     </div>
                                     <div className = "form-group">
                                         <label> Password: </label>
                                         <input placeholder="password" name="password" className="form-control" 
-                                            value={password} onChange={changePasswordHandler}/>
+                                            value={password} onChange={changePasswordHandler} minLength='4' required/>
                                     </div>
                                   
     
